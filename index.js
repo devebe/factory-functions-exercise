922c57b75d9db873425f561c52ea490488f1a7a1
devebe <dvbirdja@gmail.com>
2023-05-31 21:22:17 +0200

    Practice with factory functions

---
+++ index.js
@@ -0,0 +1,34 @@
+const userFactory = (name, age, address) => {
+    const printAge = () => console.log(`${name} is ${age} years old.`);
+    const printAddress = () => console.log(`${name} lives on ${address}.`);
+    return {name, age, address, printAge, printAddress};
+};
+
+const counterCreator = () => {
+    let count = 0;
+    return () => {
+        console.log(count);
+        count++;
+    };
+};
+
+const htmlCreator = (location, childId, tag, text) => {
+    const renderInDOM = () => {
+        const element = document.querySelector(location);
+        const child = document.createElement(tag);
+        child.setAttribute('id', childId);
+        child.textContent = text;
+        element.appendChild(child);
+    };
+    const removeFromDOM = () => {
+        const element = document.getElementById(childId);
+        element.remove();
+    }
+    return {
+        tag: tag, 
+        targetHTML: location, 
+        newElementId: childId, 
+        renderInDOM, 
+        removeFromDOM
+    };
+};
